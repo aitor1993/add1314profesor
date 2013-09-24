f84c2793256609b5cc561628c2c9b18bcf1797ba
David Vargas <profesor.dawar@gmail.com>
2013-09-24 09:53:14 +0100

    add more files

--- unit.1/test3.rb
+++ unit.1/test3.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
 # encoding: UTF-8
 
-a="eliminar"
+a=ARGV.first
+puts a
 
 if a=="crear" then
 	system("mkdir curso2")
@@ -10,9 +11,9 @@ if a=="crear" then
 	system("mkdir curso2/srd")
 	system("chmod 700 curso2/*")
 	puts "Carpetas creadas!"
-end
-
-if a=="eliminar" then
+elsif a=="eliminar" then
 	system("rm -r curso2")
 	puts "Carpetas eliminadas!"
+else
+	puts "No entiendo <"+a+">!"
 end
